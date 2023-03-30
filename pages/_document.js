cff878d773d437890a7c4226f3166dbf1471e2d7
xhuong <lxhuong484@gmail.com>
2023-03-30 21:56:05 +0700

    link font family to project

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
